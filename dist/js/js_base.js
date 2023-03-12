85e13cc176033b746a21f1e6ae818d260ab40e89
jiyan <orzz7.328@gmail.com>
2023-03-12 16:01:42 +0800

    1

---
+++ dist/js/js_base.js
@@ -0,0 +1,9 @@
+let age = 19;
+console.log("age", age);
+
+let firstName = "Chen";
+let lastName = "Jiyan";
+let fullName = firstName + " " + lastName;
+console.log("fullName", fullName);
+let fullName2 = `${firstName} ${lastName}`; // ES6 用法，可讀性較高
+console.log("fullName2", fullName2);
